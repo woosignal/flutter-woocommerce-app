85d47226eedbecf798ec8420c9bd3812e429ef1b
Anthony <ants52@aol.com>
2021-02-21 22:49:50 +0000

    remove unused import

--- LabelStoreMax/lib/pages/checkout_shipping_type.dart
+++ LabelStoreMax/lib/pages/checkout_shipping_type.dart
@@ -8,8 +8,6 @@
 //  distributed under the License is distributed on an "AS IS" BASIS,
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:label_storemax/app_state_options.dart';
 import 'package:label_storemax/helpers/tools.dart';
