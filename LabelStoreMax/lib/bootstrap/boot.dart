9ad8910f4d0503f167bb112be188ca9c79e0339a
Anthony <ants52@aol.com>
2022-01-07 14:07:42 +0000

    v5.6.2 - updates

--- LabelStoreMax/lib/bootstrap/boot.dart
+++ LabelStoreMax/lib/bootstrap/boot.dart
@@ -1,5 +1,6 @@
 // import 'package:firebase_core/firebase_core.dart';
 // import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:flutter_app/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/bootstrap/app_helper.dart';
